e90a8ad5129771a3b87c5f632c5cc10a678c7f15
Matthew2839 <matthew2839@postech.ac.kr>
2025-11-20 00:25:29 +0900

    feat & refactor: add four environment variables and change name of some vals
    feat
    four environment variables
    masterIp, masterPort, workerIp, workerPort
    
    refactor
    val channel -> channleToMaster
    val host, port -> proper variables with the env vars

--- worker/src/main/scala/Worker.scala
+++ worker/src/main/scala/Worker.scala
@@ -8,27 +8,32 @@ import proto.common._                  // WorkerData, WorkerDataResponse 등
 import proto.common.MasterServiceGrpc  // gRPC 서비스 바인딩
 
 object Worker extends App {
-  val host = "localhost"
-  val port = 9000
 
-  val channel =
+  val masterIp    = sys.env("MASTER_IP")
+  val masterPort  = sys.env("MASTER_PORT").toInt
+  val workerIp      = sys.env("WORKER_IP")
+  val workerPort  = sys.env("WORKER_PORT").toInt
+  //val masterIp = "localhost"  // 로컬 테스트용 마스터 host
+  //val masterPort = 9000         // 로컬 테스트용 마스터 port
+
+  val channelToMaster =
     ManagedChannelBuilder
-      .forAddress(host, port)
+      .forAddress(masterIp, masterPort)
       .usePlaintext() // TLS 안 씀
       .build()
 
   val stub: MasterServiceGrpc.MasterServiceStub =
-    MasterServiceGrpc.stub(channel)
+    MasterServiceGrpc.stub(channelToMaster)
 
-  println(s"[WORKER] Connecting to master at $host:$port ...")
+  println(s"[WORKER] Connecting to master at $masterIp:$masterPort ...")
 
   val request = WorkerData(
     fileSize = 123456L,
-    workerHost = host,
-    workerPort = port
+    workerHost = workerIp,
+    workerPort = workerPort
   )
 
-  println("[WORKER] Sending SendWorkerData RPC...")
+  println("[WORKER] Registering with registerWorker RPC...")
   val responseF: Future[WorkerDataResponse] =
     stub.registerWorker(request)
 
@@ -36,5 +41,5 @@ object Worker extends App {
 
   println("[WORKER] RPC finished successfully.")
 
-  channel.shutdownNow()
+  channelToMaster.shutdownNow()
 }
