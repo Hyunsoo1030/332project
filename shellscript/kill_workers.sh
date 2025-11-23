a30096aafab991f460c673879f940d48345c758f
Matthew2839 <matthew2839@postech.ac.kr>
2025-11-24 01:19:25 +0900

    chore: remove unused shellscript files

--- shellscript/kill_workers.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-for i in $(seq 101 120); do
-  IP="2.2.2.$i"
-  echo "=== killing on $IP ==="
-  ssh navy@$IP 'pkill -f "worker.jar" || echo "no worker.jar"'
-done
