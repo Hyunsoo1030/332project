c69bb4955be0b30da9655f889b526d96c2d8975b
Matthew2839 <matthew2839@postech.ac.kr>
2025-11-29 04:38:42 +0900

    feat & refactor: Implement a required execution way using args and ordered worker informations sending
    feat
    과제 요구사항처럼 실행할 수 있도록(master 3) args reading 구현 및 이를 위해
    1. 본인 Ip를 구하는 findMtIp() 구현 및 val host에 적용
    2. rpc getNewPort 구현을 통해 로컬에서도 과제 요구사항처럼 실행시켰을 때 포트 겹침 방지
    rpc notifyConnection 구현으로 테스트 시 채널 연결 시점 확인 가능
    Shuffling 단계를 위해 워커의 sendPivots를 호출할 때 워커의 순서와 다른 워커들의 정보까지 전달
    
    refactor
    기존에 object Master 안에 그대로 있던 Sampling 코드를 doSample()에 넣고 호출하는 방식으로 변경

--- master/src/main/scala/Master.scala
+++ master/src/main/scala/Master.scala
@@ -6,14 +6,21 @@ import proto.common.MasterServiceGrpc
 import proto.common.WorkerServiceGrpc
 import proto.common.WorkerServiceGrpc.WorkerServiceStub
 
+import java.net._
+import scala.jdk.CollectionConverters._
 import scala.collection.mutable
 import scala.collection.mutable.ListBuffer
-import scala.concurrent.duration._
-import scala.concurrent.{Await, ExecutionContext, Future, Promise}
+import scala.concurrent.{ExecutionContext, Future, Promise}
 import scala.io.StdIn
 import scala.util.{Failure, Success}
 
 object Master extends App {
+  // 입력 요구 사항
+  if (args.length != 1) {
+    System.err.println("Usage: master <#workers>")
+    sys.exit(1)
+  }
+  val numWorkers = args(0).toInt
   implicit val ec: ExecutionContext = ExecutionContext.global
 
   case class WorkerEntry
@@ -27,6 +34,7 @@ object Master extends App {
   val workers: mutable.ListBuffer[WorkerEntry]
   = mutable.ListBuffer.empty[WorkerEntry]
 
+  val host = findMyIp()
   val port = 8915
   val server: Server =
     ServerBuilder
@@ -35,89 +43,10 @@ object Master extends App {
       .build()
       .start()
 
-  println(s"[MASTER] gRPC server started, listening on $port")
+  println(s"[MASTER] gRPC server started, listening on $host:$port")
 
   val whenRegisterCompleted = Promise[Unit]()
-
-  //sampling phase
-  var samplesF: ListBuffer[Future[SampleResponse]] = ListBuffer.empty[Future[SampleResponse]]
-  val whenSampleAcquired = Promise[Unit]()
-  whenRegisterCompleted.future.onComplete(_ => {
-    var succeedNum = 0
-    samplesF = for (w <- workers) yield {
-      val sampleF = w.stub.getSamples(SampleRequest())
-        sampleF.onComplete{
-          case Success(pivots) => {
-            println(s"[MASTER] getSamples from ${w.host}:${w.port} succeeded." +
-              s"\n[MASTER] Pivot samples: ${pivots.samples.take(5)}")
-            succeedNum += 1
-            if (succeedNum == workers.length) whenSampleAcquired.trySuccess(())
-          }
-          case Failure(e) =>
-            println(s"[MASTER] getSamples from ${w.host}:${w.port} failed: ${e.getMessage}")
-        }
-      sampleF
-    }
-  })
-
-  whenSampleAcquired.future.onComplete(_ -> { //Sample data 전부 받아왔을 때 출력 -> 다음으로 sort 시작
-    println("==========================\n" +
-            "[MASTER] Samples acquired." +
-          "\n==========================")
-  })
-
-  // 마스터에서 직접 정렬하는 단계
-  val allPivotsF: Future[Seq[SampleResponse]] =
-    whenSampleAcquired.future.flatMap { _ =>
-      // 여기서 samplesF: ListBuffer[Future[Pivots]] 를 한 번에 모아서
-      Future.sequence(samplesF.toSeq)    // Future[Seq[Pivots]]
-    }
-
-  val whenPivotsGenerated = Promise[IndexedSeq[String]]()
-  allPivotsF.onComplete {
-    case Success(pivotsSeq) =>
-      // pivotsSeq: Seq[Pivots]
-      // 각 worker의 pivots를 전부 모아서 flatten
-      val allKeys: Seq[String] = pivotsSeq.flatMap(_.samples)
-
-      // 이제 Sampling 단계: master에서 정렬
-      val sortedKeys: Seq[String] = allKeys.sorted
-
-      println(s"[MASTER] total sampled keys = ${allKeys.size}")
-      println(s"[MASTER] first few sorted sample keys = ${sortedKeys.take(10)}")
-
-      // 여기서 global pivot 뽑기 (예: 워커 수 - 1 개 선택)
-      val numWorkers = workers.size
-      if (numWorkers > 1 && sortedKeys.nonEmpty) {
-        val step = sortedKeys.size.toDouble / (numWorkers)
-        val globalPivots =
-          (1 until numWorkers).map(i => sortedKeys((i * step).toInt))
-
-        println(s"[MASTER] global pivots = $globalPivots")
-        whenPivotsGenerated.trySuccess(globalPivots)
-
-      }
-
-    case Failure(e) =>
-      println(s"[MASTER] allPivotsF failed: ${e.getMessage}")
-  }
-
-  // 워커로 Pivots 전송하는 단계
-  whenPivotsGenerated.future.onComplete{pv =>
-    val sendPivotFutures: List[Future[PivotResponse]] =
-      workers.map { w =>
-        w.stub.sendPivots(PivotRequest(pivots=pv.get))
-      }.toList
-    Future.sequence(sendPivotFutures).onComplete { _ =>
-      println("[MASTER] All pivots are sent to workers.")
-      println("=================================\n" +
-              "[MASTER] Sampling phase finished." +
-            "\n=================================")
-      println("Press ENTER to terminate master server.")
-    }
-  }
-
-
+  doSample()
 
   // 비상용 종료 플로우 - 서버만 종료
   sys.addShutdownHook {
@@ -147,11 +76,121 @@ object Master extends App {
     println("[MASTER] Master terminated.")
   }
 
+  def doSample(): Future[Unit] = Future {
+    //sampling phase
+    var samplesF: ListBuffer[Future[SampleResponse]] = ListBuffer.empty[Future[SampleResponse]]
+    val whenSampleAcquired = Promise[Unit]()
+    whenRegisterCompleted.future.onComplete(_ => {
+      var succeedNum = 0
+      samplesF = for (w <- workers) yield {
+        val sampleF = w.stub.getSamples(SampleRequest())
+        sampleF.onComplete{
+          case Success(pivots) => {
+            println(s"[MASTER] getSamples from ${w.host}:${w.port} succeeded." +
+              s"\n[MASTER] Pivot samples: ${pivots.samples.take(5)}")
+            succeedNum += 1
+            if (succeedNum == workers.length) whenSampleAcquired.trySuccess(())
+          }
+          case Failure(e) =>
+            println(s"[MASTER] getSamples from ${w.host}:${w.port} failed: ${e.getMessage}")
+        }
+        sampleF
+      }
+    })
+
+    whenSampleAcquired.future.onComplete(_ -> { //Sample data 전부 받아왔을 때 출력 -> 다음으로 sort 시작
+      println("=====================================================\n" +
+              "[MASTER] All samples acquired. Start sorting samples." +
+            "\n=====================================================")
+    })
+
+    // 마스터에서 직접 정렬하는 단계
+    val allPivotsF: Future[Seq[SampleResponse]] =
+      whenSampleAcquired.future.flatMap { _ =>
+        // 여기서 samplesF: ListBuffer[Future[Pivots]] 를 한 번에 모아서
+        Future.sequence(samplesF.toSeq)    // Future[Seq[Pivots]]
+      }
+
+    val whenPivotsGenerated = Promise[IndexedSeq[String]]()
+    allPivotsF.onComplete {
+      case Success(pivotsSeq) =>
+        // pivotsSeq: Seq[Pivots]
+        // 각 worker의 pivots를 전부 모아서 flatten
+        val allKeys: Seq[String] = pivotsSeq.flatMap(_.samples)
+
+        // 이제 Sampling 단계: master에서 정렬
+        val sortedKeys: Seq[String] = allKeys.sorted
+
+        println(s"[MASTER] total sampled keys = ${allKeys.size}")
+        println(s"[MASTER] first few sorted sample keys = ${sortedKeys.take(10)}")
+
+        // 여기서 global pivot 뽑기 (예: 워커 수 - 1 개 선택)
+        val numWorkers = workers.size
+        if (numWorkers > 1 && sortedKeys.nonEmpty) {
+          val step = sortedKeys.size.toDouble / (numWorkers)
+          val globalPivots =
+            (1 until numWorkers).map(i => sortedKeys((i * step).toInt))
+
+          println(s"[MASTER] global pivots = $globalPivots")
+          whenPivotsGenerated.trySuccess(globalPivots)
+
+        }
+
+      case Failure(e) =>
+        println(s"[MASTER] allPivotsF failed: ${e.getMessage}")
+    }
+
+    // 워커로 Pivots 전송하는 단계
+    whenPivotsGenerated.future.onComplete{pv =>
+      val sendPivotFutures: List[Future[PivotResponse]] =
+        workers.map { w =>
+          w.stub.sendPivots(PivotRequest(
+            pivots=pv.get,myOrder=workers.indexOf(w), orderedWorkerData=workers.map(w => OrderedWorkerData(
+              order=workers.indexOf(w), workerHost=w.host, workerPort=w.port
+            )).toList
+          ))
+        }.toList
+      Future.sequence(sendPivotFutures).onComplete { _ =>
+        println("[MASTER] All pivots are sent to workers.")
+        println("=================================\n" +
+                "[MASTER] Sampling phase finished." +
+              "\n=================================")
+        println("Press ENTER to terminate master server.")
+      }
+    }
+  }
+
+  def findMyIp(): String = {
+    val nets = NetworkInterface.getNetworkInterfaces.asScala.toList
+
+    val addrs =
+      for {
+        net <- nets
+        if net.isUp && !net.isLoopback && !net.isVirtual
+        addr <- net.getInetAddresses.asScala
+        if addr.isInstanceOf[Inet4Address]
+      } yield addr.getHostAddress
+
+    addrs.headOption.getOrElse("127.0.0.1")
+  }
 }
+
+
 
 // 실제 RPC 구현체
 class MasterServiceImpl(implicit ec: ExecutionContext)
   extends MasterServiceGrpc.MasterService {
+
+  private var nextPort: Int = 50509
+
+  override def getNewPort(req: PortRequest): Future[PortResponse] = Future {
+    val p = this.synchronized {
+      val cur = nextPort
+      nextPort += 1
+      cur
+    }
+    PortResponse(portNum = p)
+  }
 
   override def registerWorker(req: WorkerData): Future[WorkerDataResponse] = {
     println(
@@ -166,15 +205,16 @@ class MasterServiceImpl(implicit ec: ExecutionContext)
 
     val stub = WorkerServiceGrpc.stub(channel)
 
+    stub.notifyConnection(ConnectionRequest(host=Master.host, port=Master.port))
+
     Master.workers += Master.WorkerEntry(req.workerHost, req.workerPort, channel, stub)
 
     println(
-      s"[MASTER] channel with worker${Master.workers.length} " +
-        s"registered at ${req.workerHost}:${req.workerPort}."
+      s"[MASTER] Worker${Master.workers.length-1} with address ${req.workerHost}:${req.workerPort} registered."
     )
 
     //등록 완료되면 sampling phase 시작하기 위함
-    if (Master.workers.length == 2) { // test 위해 값 바꿀 수 있음, default 값은 20 유지
+    if (Master.workers.length == Master.numWorkers) { // test 위해 값 바꿀 수 있음, default 값은 20 유지
       println("================================================================\n" +
               "[MASTER] All workers have registered. Initialize sampling phase." +
             "\n================================================================")
@@ -184,4 +224,10 @@ class MasterServiceImpl(implicit ec: ExecutionContext)
     // registerWorker 의 응답은 그냥 바로 성공 리턴
     Future.successful(WorkerDataResponse())
   }
+
+  override def notifyConnection(req: ConnectionRequest): Future[ConnectionResponse] = Future {
+    println(s"[MASTER] Connected from ${req.host}:${req.port}.")
+    ConnectionResponse()
+  }
+
 }
